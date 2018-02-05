77f662a02254e92851d4db34623ee7ea20610d21
Vincent Fazio <vjf@users.noreply.github.com>
2018-02-06 09:14:18 +1100

    AUS-2981 Fixed up LHS layer control panel

--- examples/NorthGawlerModel.js
+++ examples/NorthGawlerModel.js
@@ -1,60 +1,78 @@
 /* global itowns, document, proj4 */
 // # Planar (EPSG:28352) viewer
 
+// TODO: 1) Add Angular 4 & decent Angular 4 template
+//       2) Trackball-like control of model
+//
 
-var extent;
+
+// iTowns extent object
+var extentObj;
+
+// div where the 3d objects are displayed
 var viewerDiv;
+
+// view object
 var view;
+
+// scene object
 var scene;
+
+// Array of {scene, checkbox, group name} objects used by model controls div
 var sceneArr;
+
+//
 var ulElem;
+
+// camera object
 var camera;
+
+// renderer object
 var renderer;
+
+// track ball controls object
 var trackBallControls;
+
+// raycaster object
 var raycaster;
+
+// mouse object
 var mouse = new THREE.Vector2();
-var groupObj = {};
-var groupObj = {};
 
-function add_cubes(scene) {
-    
-    var geometry = new THREE.BoxGeometry( 5000, 5000, 5000 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    var cube = new THREE.Mesh( geometry, material );
-    cube.position.copy(new itowns.THREE.Vector3(extent.west(), extent.south(), 10000));
-    cube.name="Red cube";
-    scene.add(cube);
+// Keeps track of which objects have been loaded into model controls
+var groupDoneObj = {};
 
-    var geometry = new THREE.BoxGeometry( 5000, 5000, 5000 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    cube.position.copy(new itowns.THREE.Vector3(extent.west(), extent.south(), 0));
-    cube.name="Green cube";
-    scene.add(cube);
-
-    var geometry = new THREE.BoxGeometry( 5000, 5000, 5000 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
-    var cube = new THREE.Mesh( geometry, material );
-    cube.position.copy(new itowns.THREE.Vector3(extent.west(), extent.south(), -20000));
-    cube.name="Blue cube";
-    scene.add(cube);
-
-    var geometry = new THREE.BoxGeometry( 5000, 5000, 5000  );
-    var material = new THREE.MeshBasicMaterial( { color: 0x777777 } );
-    var cube = new THREE.Mesh( geometry, material );
-    cube.position.copy(new itowns.THREE.Vector3(extent.west(), extent.south(), -40000));
-    cube.name="Grey cube";
-    scene.add(cube);
+function add_display_groups(groupName) {
+        // Make a group name
+        var liElem = document.createElement("li");
+        var oText = document.createTextNode(groupName);
+        liElem.appendChild(oText);
+        liElem.style = "font-weight: bold; color: rgb(255, 255, 255); list-style-type: circle;";
+        ulElem.appendChild(liElem);
+        // Make a check box for the group
+        var grpChkBox = document.createElement("input");
+        grpChkBox.setAttribute("type", "checkbox");
+        grpChkBox.setAttribute("checked", true);
+        var grpClickEvtHandler = function (groupName, sceneArr) {
+            for (var idx=0; idx<sceneArr.length; idx++) {
+                if (sceneArr[idx] && sceneArr[idx]['group']===groupName) {
+                    sceneArr[idx]['scene'].visible = !sceneArr[idx]['scene'].visible;
+                    if (!sceneArr[idx]['scene'].visible) {
+                        sceneArr[idx]['checkbox'].removeAttribute("checked");
+                    } else {
+                        sceneArr[idx]['checkbox'].setAttribute("checked", true);
+                    }
+                }
+            }
+            view.notifyChange(true);
+        };
+        grpChkBox.addEventListener("click", grpClickEvtHandler.bind(null, groupName, sceneArr));
+        liElem.appendChild(grpChkBox);
 }
 
-function add_display(part, idx, sceneObj, groupName) {
-    console.log("add_display(part=", part, "idx=", idx, "sceneObj=", sceneObj, "groupName=", groupName,")");
-    if (!(groupName in groupObj)) {
-        var oText = document.createTextNode("*** "+groupName);
-        ulElem.appendChild(oText);
-        groupObj[groupName] = true;
-    }        
+function add_display(part, idx, sceneObj, groupName) {        
     var liElem = document.createElement("li");
+    liElem.style = "color: rgb(150, 150, 150); list-style-type: square; margin-left: 6px;";
     var oText = document.createTextNode(part.display_name);
     liElem.appendChild(oText);
     var chBox = document.createElement("input");
@@ -64,11 +82,17 @@ function add_display(part, idx, sceneObj, groupName) {
     } else {
       chBox.setAttribute("checked", false);
     }
-    sceneArr[idx] = sceneObj;
-    var clickEvtHandler = function (p1) { p1.visible = !p1.visible; view.notifyChange(true); console.log("clicked on sceneObj: ", p1, p1.name); };
-    chBox.addEventListener("click", clickEvtHandler.bind(null, sceneArr[idx]));
+    sceneArr[idx] = {"scene": sceneObj, "checkbox": chBox, "group": groupName};
+    var clickEvtHandler = function (p1) { p1.visible = !p1.visible; view.notifyChange(true); };
+    chBox.addEventListener("click", clickEvtHandler.bind(null, sceneArr[idx]['scene']));
     liElem.appendChild(chBox);
-    ulElem.appendChild(liElem);
+    
+    for (var i=0; i<ulElem.childNodes.length; i++) {
+        var firstCh = ulElem.childNodes[i].firstChild;
+        if (firstCh.nodeName === "#text" && firstCh.nodeValue === groupName) {
+            ulElem.insertBefore(liElem, ulElem.childNodes[i].nextSibling);
+        }
+    }
     part.loaded = true;
 }
 
@@ -85,9 +109,9 @@ function initialise_model(config) {
 
     // Define geographic extent: CRS, min/max X, min/max Y
     // Model boundary according to the North Gawler Province Metadata PDF using projection: UTM Zone 52 Datum: GDA94 => EPSG:28352
-    extent = new itowns.Extent(props.crs, props.extent[0], props.extent[1], props.extent[2], props.extent[3]);
+    extentObj = new itowns.Extent(props.crs, props.extent[0], props.extent[1], props.extent[2], props.extent[3]);
     
-    // console.log(extent.dimensions().x, extent.dimensions().y, extent.center().x(), extent.center().y());
+    // console.log(extentObj.dimensions().x, extentObj.dimensions().y, extentObj.center().x(), extentObj.center().y());
 
     // `viewerDiv` will contain iTowns' rendering area (`<canvas>`)
     viewerDiv = document.getElementById('viewerDiv');
@@ -97,12 +121,22 @@ function initialise_model(config) {
     modelControlsDiv.appendChild(ulElem);
     
     sceneArr = [];
-    for (group=0; group<config.groups.length; group++) {
+    sceneIdx = 0;
+    groupSceneIdxObj = {};
+    for (group in config.groups) {
         var parts = config.groups[group];
+        groupSceneIdxObj[group] = []
         for (i=0; i<parts.length; i++) {
             sceneArr.push(null);
+            groupSceneIdxObj[group].push(sceneIdx);
+            sceneIdx++;
         }
+        add_display_groups(group);
     }
+    
+
+
+    
 
     /*renderer = new THREE.WebGLRenderer( { antialias: false } );
 				renderer.setPixelRatio( window.devicePixelRatio );
@@ -112,8 +146,6 @@ function initialise_model(config) {
     
     // Scene
     scene = new itowns.THREE.Scene();
-    // add_cubes(scene);
-    // profile_scene(scene);
     
 
     //var axesHelper = new itowns.THREE.AxesHelper( 5 );
@@ -126,14 +158,12 @@ function initialise_model(config) {
     var ambient = new itowns.THREE.AmbientLight(0xFFFFFF);
     ambient.name = "Ambient Light";
     scene.add(ambient);
-    // profile_scene(scene);
     
     // Point light
     var pointlight = new THREE.PointLight();
-    pointlight.position.set(extent.west(), extent.south(), 400000);
+    pointlight.position.set(extentObj.west(), extentObj.south(), 400000);
     pointlight.name = "Point Light";
     scene.add(pointlight);
-    // profile_scene(scene);
 
     // Add GLTF objects
     var manager = new itowns.THREE.LoadingManager();
@@ -152,6 +182,7 @@ function initialise_model(config) {
         console.log("GLTF/OBJ load error!", xhr);
     };
     idx=0
+    // Load GLTF objects into scene
     for (group in config.groups) {
         var parts = config.groups[group];
         // console.log("group=", group, "parts=", parts);
@@ -159,10 +190,8 @@ function initialise_model(config) {
             if (parts[i].type === "GLTFObject" && parts[i].include) {
                 (function(part, idx, group) {
                     loader.load(part.url, function (g_object) {
-                        console.log("GLTFOBJ LOADING part=", part, "idx=", idx);
                         g_object.scene.name = part.display_name;                   
                         scene.add(g_object.scene);
-                        // profile_scene(scene);
                         add_display(part, idx, g_object.scene, group);
                         check_scene_loaded(config);
                     }, onProgress, onError);
@@ -181,7 +210,6 @@ function initialise_model(config) {
         for (i=0; i<parts.length; i++) {
             if (parts[i].type === "ImagePlane" && parts[i].include) {
                 (function(part, idx, group) {
-                  console.log("part=", part, "idx=", idx);
                   var texture = textureLoader.load(part.url,
                     function (textya) {
                         textya.minFilter = THREE.LinearFilter;
@@ -189,13 +217,12 @@ function initialise_model(config) {
 	    	   	            map: textya,
                             side: THREE.DoubleSide
 	    	            } );
-                        var geometry = new THREE.PlaneGeometry(extent.dimensions().x, extent.dimensions().y);
+                        var geometry = new THREE.PlaneGeometry(extentObj.dimensions().x, extentObj.dimensions().y);
                         var plane = new THREE.Mesh(geometry, material);
-                        var position = new itowns.THREE.Vector3(extent.center().x(), extent.center().y(), part.position[2]);
+                        var position = new itowns.THREE.Vector3(extentObj.center().x(), extentObj.center().y(), part.position[2]);
                         plane.position.copy(position);
                         plane.name = part.display_name;                    
                         scene.add(plane);
-                        // profile_scene(scene);
                         add_display(part, idx, plane, group);
                         check_scene_loaded(config);
 	                },
@@ -221,7 +248,6 @@ function initialise_model(config) {
 //
 function initialise_view(config) {
     
-    console.log("initialise_view(", config, ")");
     var i;
     var props = config.properties;
     
@@ -231,16 +257,17 @@ function initialise_view(config) {
     
     /*camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.z = 50000;
-    camera.lookAt(extent.center().xyz());*/
+    camera.lookAt(extentObj.center().xyz());*/
     
     // Create an instance of PlanarView
-    view = new itowns.PlanarView(viewerDiv, extent, {renderer: renderer, scene3D: scene});
+    view = new itowns.PlanarView(viewerDiv, extentObj, {renderer: renderer, scene3D: scene});
+    
+    // Disable ugly tile skirts
     view.tileLayer.disableSkirt = true;
     
     // Add WMS layers
     for (group in config.groups) {
         var parts = config.groups[group];
-        // console.log("group=", group, "parts=", parts);
         for (i=0; i<parts.length; i++) {
             if (parts[i].type === "WMSLayer" && parts[i].include) {
                 view.addLayer({
@@ -269,24 +296,23 @@ function initialise_view(config) {
     
 
     // Set camera position above land
-    view.camera.setPosition(new itowns.Coordinates(props.crs, extent.west()-100000, extent.south()-100000, 200000));
+    view.camera.setPosition(new itowns.Coordinates(props.crs, extentObj.west()-100000, extentObj.south()-100000, 200000));
 
 
     //var helper = new THREE.CameraHelper( camera /*view.camera.camera3D*/ );
    //scene.add( helper );
     
-    // Then look at extent's center
-    view.camera.camera3D.lookAt(extent.center().xyz()); /* new itowns.THREE.Vector3(0, 0, 0) */
+    // Then look at extentObj's center
+    view.camera.camera3D.lookAt(extentObj.center().xyz()); /* new itowns.THREE.Vector3(0, 0, 0) */
 
     // Set up controls
     var planarControls = new itowns.PlanarControls(view, { maxZenithAngle: 135, 
                                                            maxAltitude: 50000000,
-                                                           extentLimit: extent,
+                                                           extentLimit: extentObj,
                                                            groundLevel: -100000,
                                                            handleCollision: false,
                                                            zoomInFactor: 0.1,
                                                            zoomOutFactor: 0.1 });
-     console.log("view.camera.camera3D=", view.camera.camera3D);
                                                            
     /*trackBallControls = new THREE.TrackballControls( camera );
 
@@ -304,37 +330,34 @@ function initialise_view(config) {
 				trackBallControls.addEventListener( 'change', function() { console.log("render()"); renderer.render(scene, camera); } );*/
     
     // Request redraw
-    console.log("2 view.notifyChange(true);");
     view.notifyChange(true);
 }
 
-function onDocumentMouseDoubleClick( event ) {
-                console.log("onDocumentMouseDoubleClick()");
+function onDocumentMouseDoubleClick(event) {
+    console.log("onDocumentMouseDoubleClick() scene.children=", scene.children);
                 
-				event.preventDefault();
+	event.preventDefault();
 
-				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+	mouse.x = (event.clientX/window.innerWidth)*2-1;
+	mouse.y = -(event.clientY/window.innerHeight)*2+1;
                 
-                raycaster.setFromCamera( mouse, view.camera.camera3D );
+    raycaster.setFromCamera(mouse, view.camera.camera3D);
 
-				var intersects = raycaster.intersectObjects( scene.children );
+	var intersects  = raycaster.intersectObjects(scene.children, true);
                 
-                console.log("intersects=", intersects);
-                /*if (intersects.length>0) {
-                    console.log("intersects[0].object=", intersects[0].object);
-                    if (intersects[0].object.name=="Bouguer Gravity" || intersects[0].object.name=="Total Magnetic Intensity - RTP") {
-                        window.open("https://sarigbasis.pir.sa.gov.au/WebtopEw/ws/samref/sarig1/image/DDD/GDP00026.pdf#page=5");
+    console.log("intersects=", intersects);
+    if (intersects.length>0) {
+        console.log("intersects[0].object=", intersects[0].object);
+        if (intersects[0].object.name=="Bouguer Gravity" || intersects[0].object.name=="Total Magnetic Intensity - RTP") {
+            window.open("https://sarigbasis.pir.sa.gov.au/WebtopEw/ws/samref/sarig1/image/DDD/GDP00026.pdf#page=5");
                         
-                    } else if (intersects[0].object.name=="2M Surface Geology") {
-                        window.open("https://sarigbasis.pir.sa.gov.au/WebtopEw/ws/samref/sarig1/image/DDD/GDP00026.pdf#page=4");
-                    } else {
-                        window.open("https://sarigbasis.pir.sa.gov.au/WebtopEw/ws/samref/sarig1/image/DDD/GDP00026.pdf#page=2");
-                    }
-                }*/
-
-
-			}
+        } else if (intersects[0].object.name=="2M Surface Geology") {
+            window.open("https://sarigbasis.pir.sa.gov.au/WebtopEw/ws/samref/sarig1/image/DDD/GDP00026.pdf#page=4");
+        } else {
+            window.open("https://sarigbasis.pir.sa.gov.au/WebtopEw/ws/samref/sarig1/image/DDD/GDP00026.pdf#page=2");
+        }
+    }
+}
 
 function render() {
 	renderer.render( scene, camera );
@@ -346,7 +369,6 @@ function check_scene_loaded(config) {
     var sceneCnt = 0;
     for (group in config.groups) {
         var parts = config.groups[group];
-        // console.log("group=", group, "parts=", parts);
         for (i=0; i<parts.length; i++) {
             if (parts[i].type !== "WMSLayer" && parts[i].include) {
                 sceneCnt++;
@@ -364,13 +386,9 @@ function check_scene_loaded(config) {
     }
 }
 
-function profile_scene(scene) {
-    console.log("scene=", scene);
-}
 
 function refresh()
 {
-    console.log("1 view.notifyChange(true);");
     view.notifyChange(true);
 }
 
@@ -383,12 +401,14 @@ function animate() {
 
 // MAIN CODE IS HERE
 
-var config = null;
+// Load and parse the file containing the model details
 var loader = new THREE.FileLoader();
 loader.load('NorthGawlerModel.json', function ( text ) {
-					config = JSON.parse( text );
+					var config = JSON.parse( text );
                     initialise_model(config);
 				} );
+                
+                
 
 exports.view = view;
 exports.scene = scene;
