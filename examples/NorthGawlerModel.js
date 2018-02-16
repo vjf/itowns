34837cdbff2e1c6dee534135f9a81172eac3184e
Vincent Fazio <vjf@users.noreply.github.com>
2018-02-16 15:35:44 +1100

    AUS-2988 Fixed image layer not appearing bug

--- examples/NorthGawlerModel.js
+++ examples/NorthGawlerModel.js
@@ -18,8 +18,8 @@ var view;
 // scene object
 var scene;
 
-// Array of {scene, checkbox, group name} objects used by model controls div
-var sceneArr;
+// Dictionary of {scene, checkbox, group name} objects used by model controls div, key is model URL
+var sceneArr = {};
 
 //
 var ulElem;
@@ -38,9 +38,6 @@ var raycaster;
 
 // mouse object
 var mouse = new THREE.Vector2();
-
-// Keeps track of which objects have been loaded into model controls
-var groupDoneObj = {};
 
 var config;
 
@@ -62,15 +59,15 @@ function add_display_groups(groupName) {
     var grpClickEvtHandler = function (event, groupName, sceneArr) {
         var new_state = event.target.checked;
         // Look for any parts that are associated with the group
-        for (var sIdx=0; sIdx<sceneArr.length; sIdx++) {
-            if (sceneArr[sIdx] && sceneArr[sIdx]['group']===groupName) {
-                sceneArr[sIdx]['scene'].visible = new_state;
+        for (sKey in sceneArr) {
+            if (sceneArr[sKey] && sceneArr[sKey]['group']===groupName) {
+                sceneArr[sKey]['scene'].visible = new_state;
                 if (!new_state) {
-                    sceneArr[sIdx]['checkbox'].checked = false;
-                    sceneArr[sIdx]['checkbox'].removeAttribute("checked");
+                    sceneArr[sKey]['checkbox'].checked = false;
+                    sceneArr[sKey]['checkbox'].removeAttribute("checked");
                 } else {
-                    sceneArr[sIdx]['checkbox'].checked = true;
-                    sceneArr[sIdx]['checkbox'].setAttribute("checked", true);
+                    sceneArr[sKey]['checkbox'].checked = true;
+                    sceneArr[sKey]['checkbox'].setAttribute("checked", true);
                 }
             }
         }
@@ -83,7 +80,7 @@ function add_display_groups(groupName) {
     liElem.appendChild(grpChkBox);
 }
 
-function add_display(part, idx, sceneObj, groupName) {        
+function add_display(part, sceneObj, groupName) { 
     var liElem = document.createElement("li");
     liElem.style = "color: rgb(150, 150, 150); list-style-type: square; margin-left: 6px;";
     var oText = document.createTextNode(part.display_name);
@@ -99,11 +96,10 @@ function add_display(part, idx, sceneObj, groupName) {
     }
     
     // Initialise sceneArr for each checkbox
-    sceneArr[idx] = {"scene": sceneObj, "checkbox": chBox, "group": groupName};
+    sceneArr[part.model_url] = {"scene": sceneObj, "checkbox": chBox, "group": groupName};
     
     // This handles the click event to show/hide for each part of the model
     var clickEvtHandler = function (event, sceneObj, groupName, chBox) {
-        // console.log("event=", event, "sceneObj=", sceneObj, "groupName=", groupName, "chBox=", chBox);
         sceneObj.visible = !sceneObj.visible;
         // Must do this update after the tickbox has been updated
         setTimeout(function() {
@@ -131,7 +127,8 @@ function add_display(part, idx, sceneObj, groupName) {
 function initialise_model(config) {
 
     var props = config.properties;
-    var i=0, group=0, idx=0;
+    var i=0;
+    var group;
     
     if (props.proj4_defn) {
         // Define projection that we will use 
@@ -153,22 +150,12 @@ function initialise_model(config) {
     modelControlsDiv.appendChild(ulElem);
     
     
-    sceneArr = [];
-    sceneIdx = 0;
+    sceneArr = {};
     
     // Add in the groups to the LHS panel
     for (group in config.groups) {
-        var parts = config.groups[group];
-        for (i=0; i<parts.length; i++) {
-            sceneArr.push(null);
-            sceneIdx++;
-        }
         add_display_groups(group);
     }
-    
-
-
-    
 
     /*renderer = new THREE.WebGLRenderer( { antialias: false } );
 				renderer.setPixelRatio( window.devicePixelRatio );
@@ -196,12 +183,15 @@ function initialise_model(config) {
     pointlight.position.set(extentObj.west(), extentObj.south(), 400000);
     pointlight.name = "Point Light";
     scene.add(pointlight);
+    
+    
+    add_planes();
 
-    // Add GLTF objects
+}
+    
+// Add GLTF objects
+function add_3dobjects() {
     var manager = new itowns.THREE.LoadingManager();
-    manager.onProgress = function ( item, loaded, total ) {
-	    //console.log( item, loaded, total );
-    };
     var loader = new THREE.GLTFLoader(manager);
     var onProgress = function ( xhr ) {
         //console.log("GLTF/OBJ onProgress()", xhr);
@@ -213,35 +203,58 @@ function initialise_model(config) {
     var onError = function ( xhr ) {
         console.log("GLTF/OBJ load error!", xhr);
     };
-    idx=0
+   
+    var promiseList = [];
+    
     // Load GLTF objects into scene
     for (group in config.groups) {
         var parts = config.groups[group];
         // console.log("group=", group, "parts=", parts);
         for (i=0; i<parts.length; i++) {
             if (parts[i].type === "GLTFObject" && parts[i].include) {
-                (function(part, idx, group) {
-                    loader.load(part.model_url, function (g_object) {
-                        g_object.scene.name = part.display_name;                   
-                        scene.add(g_object.scene);
-                        add_display(part, idx, g_object.scene, group);
-                        check_scene_loaded(config);
-                    }, onProgress, onError);
-                })(parts[i], idx, group);
+                promiseList.push( new Promise( function( resolve, reject ) {
+                    (function(part, group) {
+                        loader.load(part.model_url, function (g_object) {
+                            g_object.scene.name = part.display_name;                   
+                            scene.add(g_object.scene);
+                            add_display(part, g_object.scene, group);
+                            resolve(g_object.scene);
+                        }, onProgress, onError);
+                    })(parts[i], group);
+                }));
+                
             }
-            idx++;
         }
     }
     
+    Promise.all(promiseList).then( function( sceneObjList ) {
+       console.log("GLTFs are loaded, now init view scene=", scene);
+       initialise_view(config);
+       view.notifyChange(true);
+
+    }, function( error ) {
+        console.error( "Could not load all textures:", error );
+    });
+}
+
+
+function add_planes() {
     // Add planes
+    var manager = new itowns.THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+	    //console.log( item, loaded, total );
+    };
+    
     var textureLoader = new THREE.TextureLoader(manager);
-    idx=0;
+    
+    var promiseList = [];
+    
     for (group in config.groups) {
         var parts = config.groups[group];
-        // console.log("group=", group, "parts=", parts);
         for (i=0; i<parts.length; i++) {
             if (parts[i].type === "ImagePlane" && parts[i].include) {
-                (function(part, idx, group) {
+                promiseList.push( new Promise( function( resolve, reject ) {
+                (function(part, group) {
                   var texture = textureLoader.load(part.model_url,
                     function (textya) {
                         textya.minFilter = THREE.LinearFilter;
@@ -253,10 +266,10 @@ function initialise_model(config) {
                         var plane = new THREE.Mesh(geometry, material);
                         var position = new itowns.THREE.Vector3(extentObj.center().x(), extentObj.center().y(), part.position[2]);
                         plane.position.copy(position);
-                        plane.name = part.display_name;                    
-                        scene.add(plane);
-                        add_display(part, idx, plane, group);
-                        check_scene_loaded(config);
+                        plane.name = part.display_name; 
+                        scene.add(plane);                        
+                        add_display(part, plane, group);
+                        resolve(plane);
 	                },
 	                // Function called when download progresses
 	                function ( xhr ) {
@@ -267,15 +280,20 @@ function initialise_model(config) {
 		                console.error('An error happened loading image plane');
 	                }
                   );
-                })(parts[i], idx, group);
+                })(parts[i], group);
+                
+                }));
             }
-            idx++;
         }
     }
     
-    
-    
-    
+    Promise.all(promiseList).then( function( sceneObjList ) {
+       console.log("planes are loaded, now for GLTF objs scene=", scene);
+       add_3dobjects();
+   
+    }, function( error ) {
+        console.error( "Could not load all textures:", error );
+    });
 }
 
 // NOTA BENE: The view objects must be added AFTER all the objects that are added to the scene directly.
@@ -283,13 +301,8 @@ function initialise_model(config) {
 // other objects in the scene.
 //
 function initialise_view(config) {
-    
     var i;
     var props = config.properties;
-    
-    var plane = new THREE.Mesh();
-    plane.name = "Dummy";
-    scene.add(plane);
     
     /*camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.z = 50000;
@@ -365,19 +378,19 @@ function initialise_view(config) {
 				trackBallControls.keys = [ 65, 83, 68 ];
 				trackBallControls.addEventListener( 'change', function() { console.log("render()"); renderer.render(scene, camera); } );*/
     
-    // Request redraw
-    view.notifyChange(true);
+
 
     // Hide any parts of the model that are not ticked
-    for (var idx=0; idx<sceneArr.length; idx++) {
-        if (sceneArr[idx] && (!sceneArr[idx]['checkbox'].hasAttribute("checked") || sceneArr[idx]['checkbox'].getAttribute("checked")===false)) {
-            sceneArr[idx]['scene'].visible = false;
+    for (var sKey in sceneArr) {
+        if (!sceneArr[sKey]['checkbox'].hasAttribute("checked") || sceneArr[sKey]['checkbox'].getAttribute("checked")===false) {
+            sceneArr[sKey]['scene'].visible = false;
         }
     }
     
     // Update group tick boxes so that if one of the group is not ticked then the overall one is not ticked also
     update_group_tickbox(null);
     
+    view.notifyChange(true);
 }
 
 
@@ -410,8 +423,51 @@ function update_group_tickbox(groupName) {
     }
 }
 
+function make_popup(event, popupInfo) {
+    var popupDiv = document.getElementById("popupBoxDiv");
+    popupDiv.style.top = event.clientY;
+    popupDiv.style.left = event.clientX;
+    popupDiv.style.display = "inline";
+    while (popupDiv.firstChild) {
+        popupDiv.removeChild(popupDiv.firstChild);
+    }
+    // Make "X" for exit button in corner of popup window
+    var exitDiv = document.createElement("div");
+    exitDiv.id = "popupExitDiv";
+    exitDiv.innerHTML = "X";
+    exitDiv.onclick = function() { document.getElementById('popupBoxDiv').style.display='none'; };
+    popupDiv.appendChild(exitDiv);
+    // Make popup title
+    var hText = document.createTextNode(popupInfo['title']);
+    hText.style = "font-weight: bold; color: rgb(255, 255, 255);";
+    popupDiv.appendChild(hText);
+    // Add in popup information
+    for (key in popupInfo) {
+         if (key !=="href" && key !=="title") {
+            var liElem = document.createElement("li");
+            liElem.style = "color: rgb(150, 150, 150); list-style-type: square; margin-left: 6px;";
+            var oText = document.createTextNode(key+": "+popupInfo[key]);
+            liElem.appendChild(oText);
+            popupDiv.appendChild(liElem);
+        // Make URLs
+        } else if (key === "href") {
+            for (var hIdx=0; hIdx<popupInfo['href'].length; hIdx++) {
+                var liElem = document.createElement("li");
+                liElem.style = "color: rgb(150, 150, 150); list-style-type: square; margin-left: 6px;";
+                var oLink = document.createElement("a");
+                oLink.href = popupInfo['href'][hIdx]['URL'];
+                oLink.style = "color: rgb(190, 190, 190);";
+                oLink.innerHTML = popupInfo['href'][hIdx]['label'];
+                oLink.target = "_blank";
+                liElem.appendChild(oLink);
+                popupDiv.appendChild(liElem);
+            }
+        }
+    }
+}
+
 function onDocumentMouseDoubleClick(event) {
-    console.log("onDocumentMouseDoubleClick() scene.children=", scene.children);
+    //console.log("onDocumentMouseDoubleClick() scene.children=", scene.children);
                 
 	event.preventDefault();
 
@@ -422,66 +478,31 @@ function onDocumentMouseDoubleClick(event) {
 
 	var intersects  = raycaster.intersectObjects(scene.children, true);
                 
-    // console.log("intersects=", intersects);
+    //console.log("intersects=", intersects);
     
     // Look at all the intersecting objects to see that if any of them have information for popups
     if (intersects.length>0) {
-        var done = false;
-        for (var n=0; n<intersects.length && !done; n++) {
+        for (var n=0; n<intersects.length; n++) {
             // console.log("intersects[n]=", intersects[n]);
             if (intersects[n].object.name==="") {
                 continue;
             }
             for (group in config.groups) {
                 var parts = config.groups[group];
-                // console.log("parts=", parts);
-                for (i=0; i<parts.length && !done; i++) {
-                    if (intersects[n].object.name === parts[i]["3dobject_label"]+"_0") {
-                        var popupDiv = document.getElementById("popupBoxDiv");
-                        popupDiv.style.top = event.clientY;
-                        popupDiv.style.left = event.clientX;
-                        popupDiv.style.display = "inline";
-                        while (popupDiv.firstChild) {
-                            popupDiv.removeChild(popupDiv.firstChild);
-                        }
-                        // Make "X" for exit button in corner of popup window
-                        var exitDiv = document.createElement("div");
-                        exitDiv.id = "popupExitDiv";
-                        exitDiv.innerHTML = "X";
-                        exitDiv.onclick = function() { document.getElementById('popupBoxDiv').style.display='none'; };
-                        popupDiv.appendChild(exitDiv);
-                        var popupInfo = parts[i].popup_info;
-                        // Make popup title
-                        var hText = document.createTextNode(popupInfo['title']);
-                        hText.style = "font-weight: bold; color: rgb(255, 255, 255);";
-                        popupDiv.appendChild(hText);
-                        // Add in popup information
-                        for (key in popupInfo) {
-                            if (key !=="href" && key !=="title") {
-                                var liElem = document.createElement("li");
-                                liElem.style = "color: rgb(150, 150, 150); list-style-type: square; margin-left: 6px;";
-                                var oText = document.createTextNode(key+": "+popupInfo[key]);
-                                liElem.appendChild(oText);
-                                popupDiv.appendChild(liElem);
-                            // Make URLs
-                            } else if (key === "href") {
-                                for (var hIdx=0; hIdx<popupInfo['href'].length; hIdx++) {
-                                    var liElem = document.createElement("li");
-                                    liElem.style = "color: rgb(150, 150, 150); list-style-type: square; margin-left: 6px;";
-                                    var oLink = document.createElement("a");
-                                    oLink.href = popupInfo['href'][hIdx]['URL'];
-                                    oLink.style = "color: rgb(190, 190, 190);";
-                                    oLink.innerHTML = popupInfo['href'][hIdx]['label'];
-                                    oLink.target = "_blank";
-                                    liElem.appendChild(oLink);
-                                    popupDiv.appendChild(liElem);
-                                }
+                for (i=0; i<parts.length; i++) {
+                    if (parts[i].hasOwnProperty("popups")) {
+                        for (popup_key in parts[i]["popups"]) {
+                            if (popup_key+"_0" === intersects[n].object.name) {
+                                make_popup(event, parts[i]["popups"][popup_key]);
+                                return;
                             }
                         }
-                        done = true;
+                    } else if (parts[i].hasOwnProperty('3dobject_label') && parts[i].hasOwnProperty('popup_info') && intersects[n].object.name === parts[i]["3dobject_label"]+"_0") {
+                        make_popup(event, parts[i]['popup_info']);
+                        return;
                     } else if (parts[i].hasOwnProperty('3dobject_label') && intersects[n].object.name === parts[i]["3dobject_label"] && parts[i].hasOwnProperty('reference')) {
                         window.open(parts[i]['reference']);
-                        done = true;
+                        return;
                     }
                 }
             }
@@ -492,32 +513,6 @@ function onDocumentMouseDoubleClick(event) {
 function render() {
 	renderer.render( scene, camera );
 }
-
-function check_scene_loaded(config) {
-    var i;
-    var loadedCnt = 0;
-    var sceneCnt = 0;
-    for (group in config.groups) {
-        var parts = config.groups[group];
-        for (i=0; i<parts.length; i++) {
-            if (parts[i].type !== "WMSLayer" && parts[i].include) {
-                sceneCnt++;
-                if (parts[i].loaded) {
-                    loadedCnt++;
-                }
-            }
-        }
-    }
-    console.log("loadedCnt, sceneCnt ", loadedCnt, sceneCnt);
-    
-    // Once parts of model are loaded we can continue with initialisation
-    if (loadedCnt===sceneCnt) {    
-        initialise_view(config);
-        // render();
-        // animate();
-    }
-}
-
 
 function refresh()
 {
